307d4cf773fb905aa9ac039883e68c9416d1c9c8
mvalnet <milla.valnet@ens.fr>
2024-06-18 16:32:40 +0200

    Update command parsing

--- ocamlrepair.ml
+++ ocamlrepair.ml
@@ -10,50 +10,32 @@ exception Not_implemented
 
 (* ______ COMMAND SETUP ______ *)
 let usage_msg =
-  "repair <file.ml> -c \"<command>\" -reg <file.txt> -err <file.txt> [-o <output>]"
+  "repair <file> -c \"<command>\" -err <test> [-o <output>]  -reg [<test1>] ..."
 
 let input_file = ref ""
 let command = ref ""
 let output_file = ref ""
-let file = ref ""
+let test = ref ""
 let err = ref ""
 let tests = ref []
-let anon_fun _ = ()
+let anon_fun filename = input_file := filename
+
 
 let spec_list =
   [
     ("-c", Arg.Set_string command, "Set command");
     ("-o", Arg.Set_string output_file, "Set output file/folder");
-    ("-reg", Arg.Set_string file, "Set regression files");
-    ("-e", Arg.Set_string err, "Set buggy file");
+    ("-reg", Arg.Rest_all (fun str_l -> tests := str_l), "Set regression files");
+    ("-err", Arg.Set_string err, "Set buggy file");
   ]
 
 let () = Arg.parse spec_list anon_fun usage_msg
 
-(* let all_euristics =
-  List.fold_left
-    (fun minimizers m -> Smap.add m.minimizer_name m minimizers)
-    Smap.empty
-    [
-
-    ] *)
 
 let default_iteration =
   [
     "replace-greater";
   ]
-(*
-let minimizers_to_run =
-  List.map
-    (fun name ->
-      try Smap.find name all_minimizers
-      with Not_found ->
-        Format.eprintf "Minimizer %S not found@." name;
-        exit 1)
-    (if !arg_minimizers = "" then default_iteration
-    else String.split_on_char ',' !arg_minimizers)
- *)
-
 
 let update_single name str =
   let oc = open_out name in
@@ -61,14 +43,6 @@ let update_single name str =
   output_string oc parse_tree;
   flush oc;
   close_out oc
-
-(* ______ ONE FILE MINIMIZATION ______ *)
-
-(** [one_file_minimize c map file] minimizes [file] in the file set [map]
-  regarding to the command [c] *)
-(* let one_file_minimize c structure file minimizer : structure * bool
-    =
-      apply_repair false file minimizer c *)
 
 type 'a step_result =
   | Repaired of 'a (* New (smaller) states that produces an error *)
@@ -103,13 +77,15 @@ let repair_basic (state : 'a) (f : 'a -> pos:int -> 'a step_result)
     (nfile, pos >= !r)
 
     let check_one_file file cmd =
+      Format.printf "trying to open %s@." file ; 
       let cin = open_in file in
+      Format.printf "opened@." ; 
       let provided_input = input_line cin in
       let expected_output = input_line cin in
       close_in cin;
       let cmd_plus_arg = Format.sprintf "%s %s" cmd provided_input in
       let pin = Unix.open_process_in cmd_plus_arg in
-      let actual_output = input_line pin in
+      let actual_output = try input_line pin with End_of_file -> "-1" in
       close_in pin;
       String.equal expected_output actual_output
 
@@ -125,8 +101,9 @@ let repair_basic (state : 'a) (f : 'a -> pos:int -> 'a step_result)
     let new_file, changed = repair_at repairer cur_file ~pos in
     let r =
       if changed then (
+        Format.printf "name is %s @." name ; 
         update_single name new_file;
-        if oracle c [] then (Repaired new_file)
+        if oracle c (!err :: !tests) then (Repaired new_file)
         else Unrelevant_change)
       else No_possible_reparation
     in
@@ -147,7 +124,7 @@ let extract_cmt = function
 let main () =
   (* PARSING COMMAND AND READING FILES*)
   let input = !input_file in
-  let cmt_command = !command^" --bin-annot -stop-after-typing "^ input in
+  let cmt_command = "ocamlc -bin-annot "^ input in
   let _ = Sys.command cmt_command in
   let cmt_infos = read_cmt (String.sub input 0 (String.length input - 3) ^ ".cmt") in
   let input_struct = ref (extract_cmt cmt_infos.cmt_annots) in
@@ -159,12 +136,13 @@ let main () =
       else !output_file
     in
 
-    let c = !command ^ " " ^ output_file in
+    let c = !command in
     update_single output_file !input_struct;
+    Format.printf "output_file is : %s" output_file;
 
     let repaired = ref false in
     while not !repaired do
-      let a, b = repair_basic !input_struct (step_minimizer c Repairer.repaire_large_to_strict output_file) in
+      let a, b = repair_basic !input_struct (step_minimizer c Repairer.repair_strict_to_large output_file) in
       input_struct := a;
       repaired := b;
     done;
--- repairer.ml
+++ repairer.ml
@@ -2,7 +2,7 @@ type ('a, 'b) t = {
   repairer_name : string;
   repairer_func : (unit -> bool) -> 'a -> 'a;
 }
-
+(* 
 let strict_to_large should_apply =
   let mapper =
     {
@@ -30,9 +30,9 @@ let strict_to_large should_apply =
   mapper.structure mapper
 
 let repaire_strict_to_large =
-  { repairer_name = "strict_to_large"; repairer_func = strict_to_large }
+  { repairer_name = "strict_to_large"; repairer_func = strict_to_large } *)
 
-let large_to_strict should_apply =
+let strict_to_large should_apply =
   let mapper =
     {
       Tast_mapper.default with
@@ -42,15 +42,17 @@ let large_to_strict should_apply =
           | Typedtree.Texp_ident (li, loc, val_desc) -> (
               match li with
               | Path.Pdot (p, s) ->
-                  if String.equal s "<=" then
-                    if should_apply () then
+(*                   Format.printf "%s @." s ;
+ *)                  if String.equal s ">" then(
+(*                     Format.printf "should_apply : %b @." (should_apply ()) ; 
+ *)                    if should_apply () then
                       {
                         e with
-                        exp_desc =
+                        exp_desc =  (
                           Typedtree.Texp_ident
-                            (Path.Pdot (p, "<"), loc, val_desc);
+                            (Path.Pdot (p, ">="), loc, val_desc));
                       }
-                    else Tast_mapper.default.expr mapper e
+                    else Tast_mapper.default.expr mapper e)
                   else Tast_mapper.default.expr mapper e
               | _ -> Tast_mapper.default.expr mapper e)
           | _ -> Tast_mapper.default.expr mapper e);
@@ -58,5 +60,5 @@ let large_to_strict should_apply =
   in
   mapper.structure mapper
 
-let repaire_large_to_strict =
-  { repairer_name = "strict_to_large"; repairer_func = large_to_strict }
+let repair_strict_to_large =
+  { repairer_name = "strict_to_large"; repairer_func = strict_to_large }
